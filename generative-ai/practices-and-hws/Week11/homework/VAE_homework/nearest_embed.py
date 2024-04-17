9b88da78d246f8171751bd04cfb5441adf83c1a2
petuch03 <egormsafronov@gmail.com>
2024-04-17 21:45:45 +0300

    generative-ai:week11:homework other required files for homework

---
+++ generative-ai/practices-and-hws/Week11/homework/VAE_homework/nearest_embed.py
@@ -0,0 +1,66 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.autograd import Function, Variable
+import torch.nn.functional as F
+
+class VectorQuantizer(nn.Module):
+    """
+    Reference:
+    [1] https://github.com/deepmind/sonnet/blob/v2/sonnet/src/nets/vqvae.py
+    """
+    def __init__(self,
+                 num_embeddings: int,
+                 embedding_dim: int,
+                 beta: float = 0.25):
+        super(VectorQuantizer, self).__init__()
+        self.K = num_embeddings
+        self.D = embedding_dim
+        self.beta = beta
+
+        self.embedding = nn.Embedding(self.K, self.D)
+        self.embedding.weight.data.uniform_(-1 / self.K, 1 / self.K)
+
+    
+    def get_encoding_inds(self, latents):
+        latents = latents.permute(0, 2, 3, 1).contiguous()  # [B x D x H x W] -> [B x H x W x D]
+        flat_latents = latents.view(-1, self.D)  # [BHW x D]
+
+        # In 1-D case each sample will be represented 
+        # as set of bottleneck // self.D discrete variables
+
+        # Compute L2 distance between latents and embedding weights
+        dist = torch.sum(flat_latents ** 2, dim=1, keepdim=True) + \
+               torch.sum(self.embedding.weight ** 2, dim=1) - \
+               2 * torch.matmul(flat_latents, self.embedding.weight.t())  # [BHW x K]
+
+        # Get the encoding that has the min distance
+        encoding_inds = torch.argmin(dist, dim=1)  # [BHW]
+        return encoding_inds, latents
+
+    def get_quantized_latents(self, encoding_inds, device):
+        encoding_inds = encoding_inds.unsqueeze(1) # [BHW, 1]
+
+        # Convert to one-hot encodings
+        encoding_one_hot = torch.zeros(encoding_inds.size(0), self.K, device=device)
+        encoding_one_hot.scatter_(1, encoding_inds, 1)  # [BHW x K]
+
+        # Quantize the latents
+        quantized_latents = torch.matmul(encoding_one_hot, self.embedding.weight)  # [BHW, D]
+        return quantized_latents
+
+    def forward(self, latents):
+        encoding_inds, latents = self.get_encoding_inds(latents)
+        quantized_latents = self.get_quantized_latents(encoding_inds, device=latents.device)
+        quantized_latents = quantized_latents.view(latents.shape)  # [B x H x W x D]
+
+        # Compute the VQ Losses
+        commitment_loss = F.mse_loss(quantized_latents.detach(), latents)
+        embedding_loss = F.mse_loss(quantized_latents, latents.detach())
+
+        vq_loss = commitment_loss * self.beta + embedding_loss
+
+        # Add the residue back to the latents
+        quantized_latents = latents + (quantized_latents - latents).detach()
+
+        return quantized_latents.permute(0, 3, 1, 2).contiguous(), vq_loss  # [B x D x H x W]
